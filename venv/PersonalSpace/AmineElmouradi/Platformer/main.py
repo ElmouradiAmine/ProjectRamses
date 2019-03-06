2d8af2b12a8aeffc0cb658f1d8cd6ba61f40dd70
Amine Elmouradi <elmouradi.amine@gmail.com>
2019-03-06 20:59:43 +0100

    Adding a platformer template

---
+++ venv/PersonalSpace/AmineElmouradi/Platformer/main.py
@@ -0,0 +1,68 @@
+
+# Game Architecture learning!
+import pygame
+import random
+from settings import *
+from colors import *
+
+class Game :
+    def __init__(self):
+        #Initialise the window ect
+        pygame.init()
+        pygame.mixer.init()
+        self.screen = pygame.display.set_mode((WIDTH,HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.playing = False
+
+    def new(self):
+        # start a new game
+        self.all_sprites = pygame.sprite.Group()
+        self.run()
+
+
+    def run(self):
+        #game loop
+        self.playing = True
+        while self.playing:
+            self.events()
+            self.update()
+            self.draw()
+            self.clock.tick(FPS)
+
+
+    def update(self):
+        self.all_sprites.update()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        pygame.display.update()
+
+    def show_start_screen(self):
+        pass
+
+    def show_gameover_screen(self):
+        pass
+
+    def close(self):
+        pygame.quit()
+        quit()
+
+
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.show_gameover_screen()
+
+g.close()
